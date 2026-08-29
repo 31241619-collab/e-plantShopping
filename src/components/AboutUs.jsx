86f225cae1a7e5b49d415e3e44c71735f07dea84
31241619-collab <31241619@vupune.ac.in>
2026-08-29 20:34:43 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        We are a company committed to delivering quality products and services
+        while ensuring customer satisfaction through innovation and excellence.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
