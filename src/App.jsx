f6f01abd3f2c6f6b136bbcf4098d5513efa92ee8
31241619-collab <31241619@vupune.ac.in>
2026-08-29 20:38:10 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App landing-page">
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery - Your one-stop destination for beautiful indoor plants.</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
